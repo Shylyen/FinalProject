65c1811f17c2b4d5099b9214afc29241c20d2f29
Shylyen <vojtechhrdla@gmail.com>
2025-01-04 12:01:22 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	event_aggregator/urls.py
    #	events/forms.py
    #	events/views.py

--- events/urls.py
+++ events/urls.py
@@ -3,7 +3,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from events.views import home
+from events.views import home, event_detail
 from events import views
 
 urlpatterns = [
@@ -15,4 +15,5 @@ urlpatterns = [
     path('accounts/register/', include('registration.backends.default.urls')),
     path('add/', views.add_event, name='add_event'),
     path("events",views.event_list,name="event_list"),
+    path("event/<pk>",event_detail,name="event_detail"),
 ]
